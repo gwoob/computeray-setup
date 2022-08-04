cba0d3dcb7f70cdf26ab5dcadcfe4b7206943f12
gwoob <47470625+gwoob@users.noreply.github.com>
2022-08-04 10:45:24 -0700

    Update services.sh

--- services.sh
+++ services.sh
@@ -8,3 +8,5 @@ ln -s /etc/sv/socklog-unix /var/service/
 ln -s /etc/sv/nanoklogd /var/service/
 ln -s /etc/sv/NetworkManager /var/service/
 ln -s /etc/sv/bluetoothd /var/service/
+ln -s /etc/sv/cupsd /var/service/
+ln -s /etc/sv/avahi-daemon /var/service/

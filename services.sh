934b65cbfc9e0c7bbf7593e03e3b3de0c120f043
gwoob <47470625+gwoob@users.noreply.github.com>
2022-08-03 22:08:03 -0700

    Update services.sh

--- services.sh
+++ services.sh
@@ -4,5 +4,7 @@ rm /var/service/dhcpcd
 rm /var/service/wicd
 rm /var/service/wpa_supplicant
 ln -s /etc/sv/openntpd /var/service/
+ln -s /etc/sv/socklog-unix /var/service/
+ln -s /etc/sv/nanoklogd /var/service/
 ln -s /etc/sv/NetworkManager /var/service/
 ln -s /etc/sv/bluetoothd /var/service/

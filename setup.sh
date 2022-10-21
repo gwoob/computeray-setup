1ebabfa270ee87122076b0bba6b7b451387e4edd
b c <47470625+gwoob@users.noreply.github.com>
2022-10-21 09:42:45 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,5 @@ cp /etc/X11/xinit/xinitrc ~/.xinitrc # copy xinitrc to correct spot
 ln -sf /usr/share/zoneinfo/los_angeles /etc/localtime # set timezone
 ln -s /bin/doas /bin/sudo # symlink doas to sudo
 chown -c root:root /etc/doas.conf && chmod 0400 /etc/doas.conf # root own doas.conf with read-only permissions
+ln -sfT dash /usr/bin/sh # symlink dash to /bin/sh
 echo "Finished."

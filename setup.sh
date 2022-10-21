a0b6b466dd316f85d6925ba82526ba6aa2261f68
b c <47470625+gwoob@users.noreply.github.com>
2022-10-21 01:26:12 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,5 @@
 xbps-remove -Rf wpa_supplicant
 cp /etc/X11/xinit/xinitrc ~/.xinitrc # copy xinitrc to correct spot
 ln -sf /usr/share/zoneinfo/los_angeles /etc/localtime # set timezone
+ln -s /bin/doas /bin/sudo # symlink doas to sudo
 echo "Finished."

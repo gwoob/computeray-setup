3cb7516f67de31eec7262c26fb8e8796f37292b9
b c <47470625+gwoob@users.noreply.github.com>
2022-10-22 01:03:37 -0700

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-ln -s /etc/sv/dhcpcd /var/service/ # dhcpd
-ln -s /etc/sv/runit-iptables /var/service/ # restores the iptables.rules and ip6tables.rules rulesets
-ln -s /etc/sv/dbus /var/service/ # dbus
-ln -s /etc/sv/openntpd /var/service/ # openntpd (openbsd network time protcol)
-ln -s /etc/sv/socklog-unix /var/service/ # socklog
-ln -s /etc/sv/nanoklogd /var/service/ # socklog
+ln -s /etc/sv/dhcpcd /var/service/default/ # dhcpd
+ln -s /etc/sv/runit-iptables /var/service/default/ # restores the iptables.rules and ip6tables.rules rulesets
+ln -s /etc/sv/dbus /var/service/default/ # dbus
+ln -s /etc/sv/openntpd /var/service/default/ # openntpd (openbsd network time protcol)
+ln -s /etc/sv/socklog-unix /var/service/default/ # socklog
+ln -s /etc/sv/nanoklogd /var/service/default/ # socklog

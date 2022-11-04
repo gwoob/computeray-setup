69b6fa5b3b1601434c8c2e160508a532c55b714a
b c <47470625+gwoob@users.noreply.github.com>
2022-11-03 23:59:47 -0700

    Update link-services.sh

--- link-services.sh
+++ link-services.sh
@@ -7,3 +7,6 @@ ln -s /etc/sv/chronyd /var/service/default/ # NTP implementation
 ln -s /etc/sv/socklog-unix /var/service/default/ # socklog
 ln -s /etc/sv/nanoklogd /var/service/default/ # socklog
 ln -s /etc/sv/dbus /var/service/default/ # dbus system bus (not to be confused with session bus)
+ln -s /etc/sv/libvirtd /var/service/default/ # libvirt
+ln -s /etc/sv/virtlockd /var/service/default/ # libvirt
+ln -s /etc/sv/virtlogd /var/service/default/ # libvirt

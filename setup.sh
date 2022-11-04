2d12dd2a0eb88f2ddcbcc37e8be28fbc489fe7f4
b c <47470625+gwoob@users.noreply.github.com>
2022-11-03 21:35:04 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 xbps-remove -Rf wpa_supplicant
 cp /etc/X11/xinit/xinitrc ~/.xinitrc # copy xinitrc to correct spot
-ln -sf /usr/share/zoneinfo/los_angeles /etc/localtime # set timezone
 ln -s /bin/doas /bin/sudo # symlink doas to sudo
 chown -c root:root /etc/doas.conf && chmod 0400 /etc/doas.conf # root own doas.conf with read-only permissions
 ln -sfT dash /usr/bin/sh # symlink dash to /bin/sh

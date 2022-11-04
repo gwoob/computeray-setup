296580e7ba2cf2f73b6aaf87edd4abe973179603
b c <47470625+gwoob@users.noreply.github.com>
2022-11-03 22:29:17 -0700

    Update link-services.sh

--- link-services.sh
+++ link-services.sh
@@ -6,3 +6,4 @@ ln -s /etc/sv/cronie /var/service/default/ # cron daemon
 ln -s /etc/sv/chronyd /var/service/default/ # NTP implementation
 ln -s /etc/sv/socklog-unix /var/service/default/ # socklog
 ln -s /etc/sv/nanoklogd /var/service/default/ # socklog
+ln -s /etc/sv/dbus /var/service/default/ # dbus system bus (not to be confused with session bus)

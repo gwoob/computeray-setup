2be8bccf521879d038e17e46f7eb6a23c5689460
b c <47470625+gwoob@users.noreply.github.com>
2022-11-03 22:44:56 -0700

    Update and rename alsa-pipewire-setup to alsa-pipewire-setup.sh

---
+++ alsa-pipewire-setup.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+mkdir -p /etc/alsa/conf.d
+ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
+ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d

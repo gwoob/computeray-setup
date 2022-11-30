302adae1615f9fa36162b46c1d2fdf708111650c
b c <47470625+gwoob@users.noreply.github.com>
2022-11-30 15:33:19 -0800

    Update link-services.sh

--- link-services.sh
+++ link-services.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 ln -s /etc/sv/dhcpcd /var/service/default/ # dhcpd
-ln -s /etc/sv/runit-iptables /var/service/default/ # restores the iptables.rules and ip6tables.rules rulesets
-ln -s /etc/sv/dbus /var/service/default/ # dbus
-ln -s /etc/sv/cronie /var/service/default/ # cron daemon
-ln -s /etc/sv/chronyd /var/service/default/ # NTP implementation
+ln -s /etc/sv/runit-iptables /var/service/default/ # restores the iptables.rules and ip6tables.rules ruleset
+ln -s /etc/sv/cronie /var/service/default/ # cron
+ln -s /etc/sv/chronyd /var/service/default/ # NTP time
 ln -s /etc/sv/socklog-unix /var/service/default/ # socklog
 ln -s /etc/sv/nanoklogd /var/service/default/ # socklog
 ln -s /etc/sv/dbus /var/service/default/ # dbus system bus (not to be confused with session bus)
@@ -11,3 +10,4 @@ ln -s /etc/sv/libvirtd /var/service/default/ # libvirt
 ln -s /etc/sv/virtlockd /var/service/default/ # libvirt
 ln -s /etc/sv/virtlogd /var/service/default/ # libvirt
 ln -s /etc/sv/acpid /var/service/default/ # ACPI daemon
+ln -s /etc/sv/alsa /var/service/default/ # alsa

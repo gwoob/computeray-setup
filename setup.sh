42f0004e121fe875da06fc7cf45c11e436041050
b c <47470625+gwoob@users.noreply.github.com>
2022-12-01 00:20:06 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ cp /etc/X11/xinit/xinitrc ~/.xinitrc # copy xinitrc to correct spot
 ln -s /bin/doas /bin/sudo # symlink doas to sudo
 chown -c root:root /etc/doas.conf && chmod 0400 /etc/doas.conf # root own doas.conf with read-only permissions
 ln -sfT dash /usr/bin/sh # symlink dash to /bin/sh
-usermod -a -G socklog ben # add user to logs group
+usermod -a -G socklog audio ben # add user to logs group and audio
 echo "Done."

f6a4ba7b9808d6ea858e959390e901e921df40b7
b c <47470625+gwoob@users.noreply.github.com>
2022-12-01 00:28:16 -0800

    Update link-services.sh

--- link-services.sh
+++ link-services.sh
@@ -2,7 +2,7 @@
 ln -s /etc/sv/dhcpcd /var/service/default/ # dhcpd
 ln -s /etc/sv/runit-iptables /var/service/default/ # restores the iptables.rules and ip6tables.rules ruleset
 ln -s /etc/sv/cronie /var/service/default/ # cron
-ln -s /etc/sv/chronyd /var/service/default/ # NTP time
+ln -s /etc/sv/openntpd /var/service/default/ # network time protocol
 ln -s /etc/sv/socklog-unix /var/service/default/ # socklog
 ln -s /etc/sv/nanoklogd /var/service/default/ # socklog
 ln -s /etc/sv/dbus /var/service/default/ # dbus system bus (not to be confused with session bus)

5f31d0a63ea5d936820f7505441ee68a389b5f04
b c <47470625+gwoob@users.noreply.github.com>
2022-12-02 00:06:34 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-cp /etc/X11/xinit/xinitrc ~/.xinitrc # copy xinitrc to correct spot
-ln -s /bin/doas /bin/sudo # symlink doas to sudo
-chown -c root:root /etc/doas.conf && chmod 0400 /etc/doas.conf # root own doas.conf with read-only permissions
-ln -sfT dash /usr/bin/sh # symlink dash to /bin/sh
+# copy xinitrc to home
+cp /etc/X11/xinit/xinitrc ~/.xinitrc
+
+# symlink doas to sudo
+ln -s /bin/doas /bin/sudo
+
+# root own doas.conf with read-only permissions
+chown -c root:root /etc/doas.conf && chmod 0400 /etc/doas.conf
+
+# symlink dash /bin/sh
+ln -sfT dash /usr/bin/sh
+
 usermod -a -G socklog audio libvirt ben # add user to logs group and audio
 echo "Done."

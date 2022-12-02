6a95341952cb216194a386d3b41e41f4d2f46165
b c <47470625+gwoob@users.noreply.github.com>
2022-12-02 00:11:21 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -12,5 +12,14 @@ chown -c root:root /etc/doas.conf && chmod 0400 /etc/doas.conf
 # symlink dash /bin/sh
 ln -sfT dash /usr/bin/sh
 
-usermod -a -G socklog audio libvirt ben # add user to logs group and audio
-echo "Done."
+# add user to socklog group m
+usermod -a -G socklog ben
+
+# add user to audio group
+usermod -a -G audio ben
+
+# add user to libvirt group
+usermod -a -G libvirt ben
+
+# add flathub flatpak repo
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo

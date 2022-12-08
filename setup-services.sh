ed3e191181ccbae4088ab660b68967c2cc4dd4c9
b c <47470625+gwoob@users.noreply.github.com>
2022-12-08 15:52:53 -0800

    Update setup-services.sh

--- setup-services.sh
+++ setup-services.sh
@@ -10,3 +10,7 @@ ln -s /etc/sv/libvirtd /var/service/default/ # libvirt
 ln -s /etc/sv/virtlockd /var/service/default/ # libvirt
 ln -s /etc/sv/virtlogd /var/service/default/ # libvirt
 ln -s /etc/sv/acpid /var/service/default/ # ACPI Daemon
+ln -s /etc/sv/snooze-hourly /var/service/default/ # snooze (cron alternative)
+ln -s /etc/sv/snooze-daily /var/service/default/
+ln -s /etc/sv/snooze-weekly /var/service/default/
+ln -s /etc/sv/snooze-monthly /var/service/default/

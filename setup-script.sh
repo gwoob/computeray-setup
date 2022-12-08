0b776d41e140c91d0cfcaf72dc05881461c2fbe0
b c <47470625+gwoob@users.noreply.github.com>
2022-12-08 15:57:51 -0800

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -3,12 +3,6 @@
 # copy xinitrc to home
 cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
-# symlink doas to sudo
-ln -s /bin/doas /bin/sudo
-
-# root own doas.conf with read-only permissions
-chown -c root:root /etc/doas.conf && chmod 0400 /etc/doas.conf
-
 # symlink dash /bin/sh
 ln -sfT dash /usr/bin/sh
 

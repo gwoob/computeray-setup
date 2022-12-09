2f8dd90d99106b9b3e0f4d053c274e96014e77a2
b c <47470625+gwoob@users.noreply.github.com>
2022-12-08 22:41:26 -0800

    Create hold-remove-packages.sh

---
+++ hold-remove-packages.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+# inside base-system
+ignorepkg=xfsprogs
+ignorepkg=f2fs-tools
+openssh
+dhcpcd
+iw
+wpa_supplicant
+nvi
+wifi-firmware
+ignorepkg=void-artwork
+
+# inside linux-firmware
+linux-firmware-broadcom
+linux-firmware-intel
+linux-firmware-qualcomm
+
+# inside xorg
+ignorepkg=xorg-video-drivers
+
+# inside xorg-apps
+oclock
+xbacklight
+xcalc
+xclock
+xcursorgen
+xeyes
+xlogo
+xmessage
+xpr
+xrefresh
+xwd
+xwud
+
+# inside xorg-input-drivers
+xf86-input-synaptics
+xf86-input-wacom
+xf86-input-vmmous
+
+
+ignorepkg=hicolor-icon-theme
+ignorepkg=mobile-broadband-provider-info
+ignorepkg=os-prober
+ignorepkg=rtkit
+
+ignorepkg=xbacklight

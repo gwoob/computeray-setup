acdc0c69b331c20c6647786aa1639f27e45fed9e
b c <47470625+gwoob@users.noreply.github.com>
2023-01-30 19:29:18 -0800

    Update setup-script.sh

--- setup-script.sh
+++ setup-script.sh
@@ -7,15 +7,7 @@ cp /etc/X11/xinit/xinitrc ~/.xinitrc
 ln -sfT dash /usr/bin/sh
 
 # add user to socklog, audio, libvirt, and plugdev groups
-usermod -a -G socklog,audio,libvirt,plugdev ben
-
-# add flathub flatpak repo
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# set optimal package repo
-mkdir -p /etc/xbps.d
-cp /usr/share/xbps.d/*-repository-*.conf /etc/xbps.d/
-sed -i 's|https://repo-us.voidlinux.org/|<repository>|g' /etc/xbps.d/*-repository-*.conf
+usermod -a -G audio,libvirt,plugdev ben
 
 # set proper timezone
 ln -sf /usr/share/zoneinfo/los_angeles /etc/localtime
@@ -24,7 +16,3 @@ ln -sf /usr/share/zoneinfo/los_angeles /etc/localtime
 mkdir -p /etc/alsa/conf.d
 ln -s /usr/share/alsa/alsa.conf.d/50-pipewire.conf /etc/alsa/conf.d
 ln -s /usr/share/alsa/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d
-
-# fix font rendering
-ln -s /usr/share/fontconfig/conf.avail/70-no-bitmaps.conf /etc/fonts/conf.d/
-xbps-reconfigure -f fontconfig

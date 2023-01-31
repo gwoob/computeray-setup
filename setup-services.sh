96688fb8a0d462b3694479daaec5f05d21cb3819
b c <47470625+gwoob@users.noreply.github.com>
2023-01-30 19:20:56 -0800

    Update setup-services.sh

--- setup-services.sh
+++ setup-services.sh
@@ -1,16 +1,8 @@
 #!/bin/sh
 
-ln -s /etc/sv/dhcpcd /var/service/default/
-ln -s /etc/sv/runit-iptables /var/service/default/ # restores iptables.rules and ip6tables.rules rulesets
-ln -s /etc/sv/cronie /var/service/default/ # cron
-ln -s /etc/sv/socklog-unix /var/service/default/ # socklog
-ln -s /etc/sv/nanoklogd /var/service/default/ # ^
-ln -s /etc/sv/dbus /var/service/default/ # dbus system bus (not to be confused with session bus)
-ln -s /etc/sv/libvirtd /var/service/default/ # libvirt
-ln -s /etc/sv/virtlockd /var/service/default/ # ^
-ln -s /etc/sv/virtlogd /var/service/default/ # ^
-ln -s /etc/sv/acpid /var/service/default/ # ACPI Daemon # zzz -H
-ln -s /etc/sv/snooze-hourly /var/service/default/ # snooze (cron alternative)
-ln -s /etc/sv/snooze-daily /var/service/default/ # ^
-ln -s /etc/sv/snooze-weekly /var/service/default/ # ^
-ln -s /etc/sv/snooze-monthly /var/service/default/ # ^
+rc-service dhcpcd start && rc-update add dhcpcd default 
+rc-service nftables start && rc-update add nftables default
+/etc/init.d/dcron start && rc-service dcron start && rc-update add dcron default
+rc-service sysklogd start && rc-update add sysklogd default
+/etc/init.d/dbus start && rc-service dbus start && rc-update add dbus default
+rc-service libvirtd start && rc-update add libvirtd default 

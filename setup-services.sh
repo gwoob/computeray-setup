e32409f508b9d9742c7604344db47c7013ad2f42
b c <47470625+gwoob@users.noreply.github.com>
2023-02-07 00:55:35 -0800

    Update setup-services.sh

--- setup-services.sh
+++ setup-services.sh
@@ -1,8 +1,4 @@
 #!/bin/sh
 
-rc-service dhcpcd start && rc-update add dhcpcd default 
 rc-service nftables start && rc-update add nftables default
-/etc/init.d/dcron start && rc-service dcron start && rc-update add dcron default
-rc-service sysklogd start && rc-update add sysklogd default
-/etc/init.d/dbus start && rc-service dbus start && rc-update add dbus default
 rc-service libvirtd start && rc-update add libvirtd default 

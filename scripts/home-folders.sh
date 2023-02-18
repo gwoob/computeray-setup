5de5ef04e6f2539eebc775cf0c9ec973a9e819a4
b c <47470625+gwoob@users.noreply.github.com>
2023-02-17 16:59:44 -0800

    Create home-folders.sh

---
+++ scripts/home-folders.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# do not run as root
+
+mkdir /home/ben/Documents
+mkdir /home/ben/Downloads
+mkdir /home/ben/Music
+mkdir /home/ben/Pictures
+mkdir /home/ben/Videos
+mkdir /home/ben/Misc.

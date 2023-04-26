a309ab2e1c9400a6c3b897558be905c9d8481c4a
b c <47470625+gwoob@users.noreply.github.com>
2023-04-25 22:57:14 -0400

    Update remove_old_kernels.sh

--- ~/bin/remove_old_kernels.sh
+++ ~/bin/remove_old_kernels.sh
@@ -1,41 +1,48 @@
 #!/bin/sh
 
 set -euo pipefail
-Check if we are running as root
 
+# Check if we are running as root
 if [ "$(id -u)" -ne 0 ]; then
-echo "This script must be run as root" >&2
-exit 1
+  echo "This script must be run as root" >&2
+  exit 1
 fi
-Clean up old kernel versions
 
-old_kernel=$(uname -r | cut -d '-' -f 1)
-if [ "${kernel}" != "${old_kernel}" ]; then
-if [ -z "${old_kernel}" ]; then
-echo "Failed to find old kernel version" >&2
-exit 1
-fi
-Backup old kernel and config file
+# Get a list of old kernel versions
+old_kernels=$(ls /usr/src/ | grep -v "$(uname -r | cut -d '-' -f 1)")
 
-cp /boot/${old_kernel} /boot/${old_kernel}.bak
-cp /usr/src/linux/.config /usr/src/linux/.config.bak
-if ! emerge -P gentoo-sources; then
-echo "Failed to remove old kernel packages" >&2
-exit 1
+# Confirm with user which files to remove
+echo "The following old kernel versions will be removed:"
+echo "${old_kernels}"
+echo "Do you want to proceed? [y/N]"
+read confirm
+if [ "${confirm}" != "y" ] && [ "${confirm}" != "Y" ]; then
+  echo "Aborted by user"
+  exit 0
 fi
-if ! rm -rf "/usr/src/linux-${old_kernel}"; then
-echo "Failed to remove old kernel sources" >&2
-exit 1
-fi
-if ! rm -rf "/boot/${old_kernel}"; then
-echo "Failed to remove old kernel files from /boot" >&2
-exit 1
-fi
-if ! rm -rf "/lib/modules/${old_kernel}"; then
-echo "Failed to remove old kernel modules" >&2
-exit 1
-fi
-fi
+
+# Remove each old kernel version and related files
+for kernel in ${old_kernels}; do
+  echo "Removing old kernel version ${kernel}"
+  cp /boot/${kernel} /boot/${kernel}.bak
+  cp /usr/src/${kernel}/.config /usr/src/${kernel}/.config.bak
+  if ! emerge -P gentoo-sources; then
+    echo "Failed to remove old kernel package for ${kernel}" >&2
+    exit 1
+  fi
+  if ! rm -rf "/usr/src/${kernel}"; then
+    echo "Failed to remove old kernel sources for ${kernel}" >&2
+    exit 1
+  fi
+  if ! rm -rf "/boot/${kernel}"; then
+    echo "Failed to remove old kernel files from /boot for ${kernel}" >&2
+    exit 1
+  fi
+  if ! rm -rf "/lib/modules/${kernel}"; then
+    echo "Failed to remove old kernel modules for ${kernel}" >&2
+    exit 1
+  fi
+done
 
 echo "Kernel cleanup completed successfully"
 exit 0

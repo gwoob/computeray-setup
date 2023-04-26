7fdde1e7e2a20db28d0d594cbb9b013eac91a501
b c <47470625+gwoob@users.noreply.github.com>
2023-04-26 02:11:26 -0400

    Update upgrade_and_poweroff.sh

--- ~/bin/upgrade_and_poweroff.sh
+++ ~/bin/upgrade_and_poweroff.sh
@@ -2,10 +2,9 @@
 # Update the system and power off the machine
 
 set -e
-set -o pipefail
 
 # Sync the portage tree and update the system
-emaint --auto sync && emerge -avuDN @world | tee emerge-log.log
+emaint --auto sync && emerge -avuDN @world > emerge-log.log 2>&1
 
 # Wait for a few seconds before powering off the machine
 sleep 5

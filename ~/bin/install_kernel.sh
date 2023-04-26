5f6561d6691db9348f56761589ef818d1f78791b
b c <47470625+gwoob@users.noreply.github.com>
2023-04-25 23:39:31 -0400

    Update install_kernel.sh

--- ~/bin/install_kernel.sh
+++ ~/bin/install_kernel.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+# This script upgrades the kernel on a Gentoo system
 
 set -euo pipefail
 
@@ -8,35 +10,46 @@ echo "This script must be run as root" >&2
 exit 1
 fi
 
-# Prompt the user to select a kernel
-eselect kernel list
-read -p "Please enter the number of the kernel you want to install: " kernel_num
-eselect kernel set $kernel_num
-Get the kernel source directory and check if the config file exists
+# Check if the 'eselect' command is available
+if ! command -v eselect >/dev/null 2>&1; then
+  echo "ERROR: 'eselect' command not found. Please install 'app-admin/eselect'." >&2
+  exit 1
+fi
 
-# requires CONFIG_IKCONFIG_PROC set
-cd /usr/src/linux >/dev/null 2>&1
+# Prompt the user to select a kernel
+echo "Available kernels:"
+eselect kernel list
+read -p "Enter the number of the kernel you want to install: " kernel_num
+if ! eselect kernel set $kernel_num >/dev/null; then
+  echo "ERROR: Invalid kernel number. Please try again." >&2
+  exit 1
+fi
+
+# Get the kernel source directory and check if the config file exists
+cd /usr/src/linux
 if [ ! -f .config ]; then
-zcat /proc/config.gz >.config
+  echo "INFO: Config file not found. Generating from /proc/config.gz..."
+  zcat /proc/config.gz >.config
 fi
 
 # Build and install the new kernel and modules
 if ! make -j$(nproc) && make -j$(nproc) modules_install; then
-echo "Failed to build or install kernel modules" >&2
-exit 1
+  echo "ERROR: Failed to build or install kernel modules." >&2
+  exit 1
 fi
 
 # Install the new kernel in /boot
-if ! make install; then
-echo "Failed to install kernel in /boot" >&2
-exit 1
+if ! make -j$(nproc) install; then
+  echo "ERROR: Failed to install kernel in /boot." >&2
+  exit 1
 fi
 
 # Rebuild any necessary modules
-if ! emerge @module-rebuild; then
-echo "Failed to rebuild necessary modules" >&2
-exit 1
+if ! sudo emerge @module-rebuild; then
+  echo "ERROR: Failed to rebuild necessary modules." >&2
+  exit 1
 fi
 
-echo "Kernel upgrade completed successfully"
+echo "INFO: Kernel upgrade completed successfully."
 exit 0
+
